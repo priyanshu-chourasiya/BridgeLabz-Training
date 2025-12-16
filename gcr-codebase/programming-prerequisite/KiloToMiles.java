fbf7e6ec45d1702fb43323eff934016186d17232
Priyanshu <priyxsh005@gmail.com>
2025-12-16 16:46:48 +0530

    [Priyanshu] : added Kilometers to miles program

---
+++ gcr-codebase/programming-prerequisite/KiloToMiles.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class KiloToMiles{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int kilo = sc.nextInt();
+		System.out.println(kilo * 0.621371);
+	}
+}
