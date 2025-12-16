407fd42aae163ee367305a5b2b0a75b355d463e4
Priyanshu <priyxsh005@gmail.com>
2025-12-16 16:43:39 +0530

    [Priyanshu] : added area of circle

---
+++ gcr-codebase/programming-prerequisite/AreaOfCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AreaOfCircle{
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	int radius = sc.nextInt();
+	double area = 3.14 * (radius * radius);
+	System.out.println(area);
+	}
+}
