2aeb6db94692e421c17c2ea30459f0ee29ae9598
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:52:31 +0530

    [Priyanshu] : Added IntOperation program

--- gcr-codebase/programming-elements/LEVEL 2/IntOperation.java
+++ gcr-codebase/programming-elements/LEVEL 2/IntOperation.java
@@ -1,10 +1,14 @@
 import java.util.*;
 public class IntOperation{
 	public static void main(String agrs[]){
+			
+			// Declaring scanner object
 			Scanner input = new Scanner(System.in);
 			
+			// declaring variables
 			int a, b, c;
 			
+			// displaying results along with calculation
 			System.out.println(a + b *c);
 			System.out.println(a * b + c);
 			System.out.println(c + a / b);
