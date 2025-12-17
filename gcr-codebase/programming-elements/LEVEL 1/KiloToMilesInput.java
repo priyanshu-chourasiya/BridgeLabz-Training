81e824791288328d0bce559d42397ba0958abbb9
Priyanshu <priyxsh005@gmail.com>
2025-12-17 14:15:26 +0530

    [Priyanshu] : Added Kilometers to miles taking user input

---
+++ gcr-codebase/programming-elements/LEVEL 1/KiloToMilesInput.java
@@ -0,0 +1,26 @@
+// importing util package to take imput from user
+import java.util.*;
+
+// declaring a class KiloToMilesInput to covvert kilometers to meters
+public class KiloToMilesInput{
+	public static void main(String args[]){
+		// declaring a variable to store kilometers value
+		double kilometers;
+		
+		// creating scanner class object to take input
+		Scanner input = new Scanner(System.in);
+		
+		// taking input of kilometers
+		kilometers = input.nextDouble();
+		
+		// declaring a variable toMiles to convert kilometers to miles
+		double toMiles;
+		
+		// converting kilometers to miles
+		toMiles = kilometers * 1.6;
+		
+		// displaying result
+		System.out.println("The total miles is " + toMiles +  " mile for the given " + kilometers + " km");
+	
+	}
+}
