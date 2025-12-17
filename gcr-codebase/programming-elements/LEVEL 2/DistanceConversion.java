527e1a54c34f5ec4c2d85172629b330b1d16b1ba
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:50:06 +0530

    [Priyanshu] : Added Distance converstion program

--- gcr-codebase/programming-elements/LEVEL 2/DistanceConversion.java
+++ gcr-codebase/programming-elements/LEVEL 2/DistanceConversion.java
@@ -2,14 +2,17 @@ import java.util.*;
 public class DistanceConversion{
 	public static void main(String args[]){
 		
+		// declaring scanner object
 		Scanner input = new Scanner(System.in);
 		
+		// taking user input
 		double distanceInFeet = input.nextDouble();
 		
-		double distanceInMiles = 3 * 1760 * distanceInFeet;
-		
+		// calculation
+		double distanceInMiles = 3 * 1760 * distanceInFeet;		
 		double distanceInYards = distanceInFeet * 3;
 		
+		// displaying the results
 		System.out.println("The distance in yards is " + distanceInYards + " while the distance in miles is " 
 							+ distanceInMiles);
 		
