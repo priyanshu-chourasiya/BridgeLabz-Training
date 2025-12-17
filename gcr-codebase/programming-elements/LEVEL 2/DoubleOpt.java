27abb8d8edf16902895c4a4dd12e0c1deacfd852
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:31:16 +0530

    [Priyanshu] : added DoubleOpt program

---
+++ gcr-codebase/programming-elements/LEVEL 2/DoubleOpt.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class DoubleOpt {
+	public static void main(String agrs[]){
+			Scanner input = new Scanner(System.in);
+			
+			Double a, b, c;
+			
+			System.out.println(a + b *c);
+			System.out.println(a * b + c);
+			System.out.println(c + a / b);
+			System.out.println(a % b + c);
+			
+			input.close();
+	}
+}
