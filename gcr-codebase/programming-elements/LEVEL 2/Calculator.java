1863a7f47ffb945d63be702cd6487991b11c8bbe
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:48:32 +0530

    [Priyanshu] : Added calculator program

--- gcr-codebase/programming-elements/LEVEL 2/Calculator.java
+++ gcr-codebase/programming-elements/LEVEL 2/Calculator.java
@@ -1,22 +1,27 @@
 import java.util.*;
 public class Calculator{
 	public static void main(String args[]){
+		
+		// declaring scanner object
 		Scanner input = new Scanner(System.in);
 		
+		// declaring variables
 		double num1 , num2;
+		double addition,subtraction,multiplication,division;
 		
+		// taking user input of variables
 		System.out.print("Enter num1 : ");
 		num1 = input.nextDouble();
 		System.out.print("Enter num2 : ");
 		num2 = input.nextDouble();
-		
-		double addition,subtraction,multiplication,division;
 
+		// Calculations
 		addition = num1 + num2;
 		subtraction = num1 - num2;
 		multiplication = num1 * num2;
 		division = num1/num2;
 		
+		// Displaying the results
 		System.out.println("The addition, subtraction, multiplication, and division values of numbers "
             + num1 + " and " + num2 + " are: "
             + addition + ", " + subtraction + ", "
