92f25daaba7b0881d3a3b3f97778688bcd3c2209
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:42:40 +0530

    [Priyanshu] : Added Kilometers to miles taking user input

--- gcr-codebase/programming-elements/LEVEL 1/DiscountPriceInput.java
+++ gcr-codebase/programming-elements/LEVEL 1/DiscountPriceInput.java
@@ -2,20 +2,18 @@ import java.util.*;
 public class DiscountPriceInput{
 	public static void main(String args[]){	
 		
+		// declaring Scanner object for input
 		Scanner input = new Scanner(System.in);
 		
+		// taking input
 		int fee = input.nextInt();
-		
-		
 		int discountPercent = input.nextInt();
 		
-		
+		// calculating discount
 		int discount;
-		
-		
 		discount = fee/discountPercent;
 		
-		
+		// printing the results
 		System.out.println("The discount amount is INR " + discount + 
 				" and final discounted fee is INR " + (fee - discount));
 	}
