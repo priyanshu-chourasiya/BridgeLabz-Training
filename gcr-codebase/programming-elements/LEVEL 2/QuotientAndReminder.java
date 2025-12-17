85349d7133160030d39a5fb2d00fd3e311ec7a57
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:53:48 +0530

    [Priyanshu] : Added QuotientAndReminder program

--- gcr-codebase/programming-elements/LEVEL 2/QuotientAndReminder.java
+++ gcr-codebase/programming-elements/LEVEL 2/QuotientAndReminder.java
@@ -2,16 +2,18 @@ import java.util.*;
 public class QuotientAndReminder{
 	public static void main(String args[]){
 		
+		// declaring scanner object
 		Scanner input = new Scanner(System.in);
 		
-		int num1 = input.nextInt();
-		
+		// taking user input
+		int num1 = input.nextInt();		
 		int num2 = input.nextInt();
 		
+		// doing calculation
 		double quotient = num1/num2;
-		
 		int reminder = num1%num2;
 		
+		// displaying the results
 		System.out.println("The Quotient is " + quotient + " and Reminder is " + reminder + 
 						" of two number " + num1 + " and " + num2);
 		
