ed37565a69c266d7f82512f0dcfdb3fa29cb57b0
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:56:33 +0530

    [Priyanshu] : Added totalAmount program

--- gcr-codebase/programming-elements/LEVEL 2/TotalAmount.java
+++ gcr-codebase/programming-elements/LEVEL 2/TotalAmount.java
@@ -2,14 +2,17 @@ import java.util.*;
 public class TotalAmount{
 	public static void main(String args[]){
 		
+		// declaring scanner object
 		Scanner input = new Scanner(System.in);
-			
-		int unitPrice = input.nextInt();
 		
+		// taking user input
+		int unitPrice = input.nextInt();		
 		int quantity = input.nextInt();
 		
+		// calculations
 		int totalPrice = unitPrice * quantity;
 		
+		// displaying the results
 		System.out.print("The total purchase price is INR " + totalPrice + 
 						" if the quantity " + quantity + " and unit price is INR " + unitPrice);
 		
