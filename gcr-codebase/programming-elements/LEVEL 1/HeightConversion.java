7868c4245ba5908db90100695c996027877f9ee2
Priyanshu <priyxsh005@gmail.com>
2025-12-17 15:44:24 +0530

    [Priyanshu] : Added height conversion program taking user input height

--- gcr-codebase/programming-elements/LEVEL 1/HeightConversion.java
+++ gcr-codebase/programming-elements/LEVEL 1/HeightConversion.java
@@ -2,16 +2,18 @@ import java.util.*;
 public class HeightConversion{
 	public static void main(String args[]){
 		
+		// declaring scanner object
 		Scanner input = new Scanner(System.in);
 		
+		// declaring variables
 		double height = input.nextDouble();
-		
 		double feet, inches;
 		
+		// calculating height in feet and inches
 		feet = height * 12 * 2.54;
-		
 		inches = height * 2.54;
-		
+
+		// displaying the results
 		System.out.println("Your Height in cm is " + height +
 				" while in feet is " + feet +" and inches is " + inches);
 		
