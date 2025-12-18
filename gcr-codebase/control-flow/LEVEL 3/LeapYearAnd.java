a3ded3a03317bb2832a9676a38dc754c94b710e5
Priyanshu <priyxsh005@gmail.com>
2025-12-18 16:26:36 +0530

    [Priyanshu] : Added LeapYearAnd using and

---
+++ gcr-codebase/control-flow/LEVEL 3/LeapYearAnd.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LeapYearAnd{
+	public static void main(String args[]){
+		
+		// declaring scanner class object
+		Scanner input = new Scanner(System.in);
+		
+		// declaring leap year varible and taking user input
+		System.out.print("Enter year : ");
+		int leapyear = input.nextInt();
+		
+		// checking conditions
+		if(leapyear >= 1582){
+			
+			// 400 modulo
+			if(leapyear%400 == 0){
+				System.out.print("LeapYear");
+			}
+			else if(leapyear%4 ==0 && leapyear%100 != 0){
+				System.out.print("LeapYear");
+			}
+			else{
+				System.out.print("Not LeapYear");
+			}
+		}
+		else{
+				System.out.print("Not LeapYear");
+			}
+		
+		// closing input
+		input.close();
+	}
+}
