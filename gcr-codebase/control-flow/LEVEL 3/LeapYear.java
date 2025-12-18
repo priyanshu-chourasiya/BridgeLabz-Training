c2a778479ce415c3c875b8206812918673cf727d
Priyanshu <priyxsh005@gmail.com>
2025-12-18 16:24:26 +0530

    [Priyanshu] : Added LeapYear using inner loops

---
+++ gcr-codebase/control-flow/LEVEL 3/LeapYear.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LeapYear{
+	public static void main(String args[]){
+		
+		// declaring scanner class object
+		Scanner input = new Scanner(System.in);
+		
+		// declaring leap year varible and taking user input
+		System.out.print("Enter year : ");
+		int leapyear = input.nextInt();
+		
+		if(leapyear >= 1582){
+			if(leapyear%400 == 0){
+				System.out.print("LeapYear");
+			}
+			else if(leapyear%4 ==0 && leapyear%100 != 0){
+				System.out.print("LeapYear");
+			}
+			else{
+				System.out.print("Not LeapYear");
+			}
+		}
+		else{
+				System.out.print("Not LeapYear");
+			}
+		
+		// closing input
+		input.close();
+	}
+}
