a5fd75ab7c575764d98bdcba8183f091098b2f0e
Priyanshu <priyxsh005@gmail.com>
2025-12-18 15:55:00 +0530

    [Priyanshu] : Added MultiplesOfNumber program

--- gcr-codebase/control-flow/LEVEL 2/MultiplesOfNumber.java
+++ gcr-codebase/control-flow/LEVEL 2/MultiplesOfNumber.java
@@ -10,8 +10,13 @@ public class MultiplesOfNumber{
 		System.out.print("Enter number : ");
 		int number = input.nextInt();
 		
+		// checking the conditions
 		if(number > 1 && number <= 100){
+			
+			// using for loop
 			for(int i = 100; i >= 1; i--){
+				
+				// checking divisibility
 				if(number%i == 0){
 					System.out.print(i + " ");
 				}
