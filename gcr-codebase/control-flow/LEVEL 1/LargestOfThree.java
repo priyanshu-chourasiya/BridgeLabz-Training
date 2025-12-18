d3092711d679c31b25aac90e9058736c93f6d1ba
Priyanshu <priyxsh005@gmail.com>
2025-12-18 12:09:20 +0530

    [Priyanshu] : Added LargestOfThree program

---
+++ gcr-codebase/control-flow/LEVEL 1/LargestOfThree.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LargestOfThree{
+	public static void main(String args[]){
+		
+		// Decalaring scanner object
+		Scanner input = new Scanner(System.in);
+		
+		// declaring three variables
+		int num1, num2, num3;
+		
+		// taking user input of three variables
+		System.out.print("Enter num1 : ");
+		num1 = input.nextInt();
+		System.out.print("Enter num2 : ");
+		num2 = input.nextInt();
+		System.out.print("Enter num3 : ");
+		num3 = input.nextInt();
+		
+		// checking the largest number among three
+		if(num1 >= num2 && num1 >= num3){
+			System.out.print("Is the first number the largest? " + true);
+		}
+		else if(num2 >= num1 & num2 >= num3){
+			System.out.print("Is the second number the largest " + true);
+		}
+		else{
+			System.out.print("Is the third number the largest? " + true);
+		}
+		
+		// closing scanner object
+		input.close();
+	}
+}
