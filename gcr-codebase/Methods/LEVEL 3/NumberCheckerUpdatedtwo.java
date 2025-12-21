c144c0fbeae487b8223b34cc805ec79bfef929e7
Priyanshu <priyxsh005@gmail.com>
2025-12-21 18:07:41 +0530

    [Priyanshu] : added NumberCheckerUpdatedtwo code

---
+++ gcr-codebase/Methods/LEVEL 3/NumberCheckerUpdatedtwo.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class NumberCheckerUpdatedtwo {
+
+    // Prime 
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Sum of digits of square == number
+    public static boolean isNeonNumber(int number) {
+        int square = number * number;
+        int sum = 0;
+
+        while (square != 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+        return sum == number;
+    }
+
+    // Spy Number
+    // Sum of digits == Product of digits
+    public static boolean isSpyNumber(int number) {
+        int sum = 0;
+        int product = 1;
+        int temp = number;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            product *= digit;
+            temp /= 10;
+        }
+        return sum == product;
+    }
+
+    // Automorphic Number
+    // Square ends with the number
+    public static boolean isAutomorphic(int number) {
+        int square = number * number;
+        int temp = number;
+
+        while (temp != 0) {
+            if (temp % 10 != square % 10) {
+                return false;
+            }
+            temp /= 10;
+            square /= 10;
+        }
+        return true;
+    }
+
+    // Buzz Number
+    public static boolean isBuzzNumber(int number) {
+        return (number % 7 == 0) || (number % 10 == 7);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        System.out.println("Is Prime Number: " + isPrime(number));
+        System.out.println("Is Neon Number: " + isNeonNumber(number));
+        System.out.println("Is Spy Number: " + isSpyNumber(number));
+        System.out.println("Is Automorphic Number: " + isAutomorphic(number));
+        System.out.println("Is Buzz Number: " + isBuzzNumber(number));
+    }
+}
