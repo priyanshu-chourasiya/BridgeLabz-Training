a7ebea28f1038df9d0719e81de2f6fa3aa7934b1
Priyanshu <priyxsh005@gmail.com>
2025-12-23 13:13:42 +0530

    [Priyanshu] : Added Palindrom code

--- gcr-codebase/strings/LEVEL 3/Palindrom.java
+++ gcr-codebase/strings/LEVEL 3/Palindrom.java
@@ -16,11 +16,24 @@ public class Palindrom{
 		return true;
 	}
 	
+	public static boolean ispalindromrecursion(String str,int st,int end){
+		if(st >= end){
+			return true;
+		}
+		if(str.charAt(st) != str.charAt(end)){
+			return false;
+		}
+		return ispalindromrecursion(str,st + 1,end - 1);
+	}
 	// main
 	public static void main(String args[]){
 		
 		String str = "abcba";
 		
-		System.out.print(ispalindrom(str));
+		System.out.println(ispalindrom(str));
+		
+		int st = 0, end = str.length() - 1;
+		
+		System.out.println(ispalindromrecursion(str,st,end));
 	}
 }
