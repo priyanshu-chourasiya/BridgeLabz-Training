ed2edbd44b9065e52514f2b5a1869baf08ceacb6
Priyanshu <priyxsh005@gmail.com>
2025-12-23 09:27:42 +0530

    [Priyanshu] : Added vote test code

---
+++ gcr-codebase/strings/LEVEL 2/VoteTest.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class VoteTest {
+
+    // Methods
+    public static int[] generateAges(int n) {
+        int[] ages = new int[n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90) + 10; // 10 to 99
+        }
+        return ages;
+    }
+
+    public static String[][] checkVoting(int[] ages) {
+        String[][] result = new String[ages.length][2];
+
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+
+            if (ages[i] < 0) {
+                result[i][1] = "false";
+            } else if (ages[i] >= 18) {
+                result[i][1] = "true";
+            } else {
+                result[i][1] = "false";
+            }
+        }
+        return result;
+    }
+
+    public static void display(String[][] data) {
+        System.out.println("Age Can Vote");
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + " " + data[i][1]);
+        }
+    }
+
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] ages = generateAges(n);
+        String[][] result = checkVoting(ages);
+        display(result);
+    }
+}
