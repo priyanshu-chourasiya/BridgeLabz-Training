920cd551a926884d3a77f386953840838630b090
Priyanshu <priyxsh005@gmail.com>
2025-12-23 12:07:59 +0530

    [Priyanshu] : Added Palindrom code

---
+++ gcr-codebase/strings/LEVEL 3/Palindrom.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Palindrom{
+	
+	// methods
+	public static boolean ispalindrom(String str){
+		
+		int st = 0, end = str.length() - 1;
+		while(st < end){
+			if(str.charAt(st) != str.charAt(end)){
+				return false;
+			}
+			st++;
+			end--;
+		}
+		return true;
+	}
+	
+	// main
+	public static void main(String args[]){
+		
+		String str = "abcba";
+		
+		System.out.print(ispalindrom(str));
+	}
+}
