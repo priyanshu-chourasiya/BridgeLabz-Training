d571c6a52c70daf66a845551e189062615ede868
Priyanshu <priyxsh005@gmail.com>
2025-12-26 22:17:41 +0530

    [Priyanshu] : Added result generator code

---
+++ scenario-based-codebase/WEEK 1/ResultGenerator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ResultGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] marks = new int[5];
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            total += marks[i];
+        }
+
+        double average = total / 5.0;
+        char grade;
+
+        switch ((int) average / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            case 6:
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+        }
+
+        System.out.println("\nAverage Marks: " + average);
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
