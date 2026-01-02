1a9329ba51b2ef4ec45a43414d04fb2ed497c02b
Priyanshu <priyxsh005@gmail.com>
2026-01-02 14:13:37 +0530

    [Priyanshu] : Added StudentRecordManagement code

--- dsa-practice/gcr-codebase/JavaDsaPactice/src/LinkedList/SinglyLL/StudentRecordManagement.java
+++ dsa-practice/gcr-codebase/JavaDsaPactice/src/LinkedList/SinglyLL/StudentRecordManagement.java
@@ -1,10 +1,118 @@
 package LinkedList.SinglyLL;
 
+class StudentNode {
+    int rollNumber;
+    String name;
+    int age;
+    String grade;
+    StudentNode next;
+
+    public StudentNode(int rollNumber, String name, int age, String grade) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
+
+class StudentLinkedList {
+    StudentNode head;
+
+    // Add at beginning
+    void addAtBeginning(int roll, String name, int age, String grade) {
+        StudentNode newNode = new StudentNode(roll, name, age, grade);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add at end
+    void addAtEnd(int roll, String name, int age, String grade) {
+        StudentNode newNode = new StudentNode(roll, name, age, grade);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        StudentNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Delete by roll number
+    void deleteByRoll(int roll) {
+        if (head == null) return;
+        if (head.rollNumber == roll) {
+            head = head.next;
+            return;
+        }
+        StudentNode temp = head;
+        while (temp.next != null) {
+            if (temp.next.rollNumber == roll) {
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    // Search by roll number
+    void searchByRoll(int roll) {
+        StudentNode temp = head;
+        while (temp != null) {
+            if (temp.rollNumber == roll) {
+                System.out.println("Roll: " + temp.rollNumber + ", Name: " + temp.name + ", Age: " + temp.age + ", Grade: " + temp.grade);
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("Student not found");
+    }
+
+    // Update grade
+    void updateGrade(int roll, String newGrade) {
+        StudentNode temp = head;
+        while (temp != null) {
+            if (temp.rollNumber == roll) {
+                temp.grade = newGrade;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    // Display all students
+    void display() {
+        StudentNode temp = head;
+        while (temp != null) {
+            System.out.println("Roll: " + temp.rollNumber + ", Name: " + temp.name + ", Age: " + temp.age + ", Grade: " + temp.grade);
+            temp = temp.next;
+        }
+    }
+}
+
 public class StudentRecordManagement {
+    public static void main(String[] args) {
+        StudentLinkedList list = new StudentLinkedList();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+        // Adding students
+        list.addAtBeginning(1, "Aman", 20, "A");
+        list.addAtEnd(2, "Ravi", 21, "B");
+        list.addAtEnd(3, "Neha", 22, "A");
 
+        System.out.println("All students:");
+        list.display();
+
+        System.out.println("\nSearch roll 2:");
+        list.searchByRoll(2);
+
+        System.out.println("\nUpdate grade of roll 3 to B+");
+        list.updateGrade(3, "B+");
+        list.display();
+
+        System.out.println("\nDelete roll 1:");
+        list.deleteByRoll(1);
+        list.display();
+    }
 }
