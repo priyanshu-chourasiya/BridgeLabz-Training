0cfe49c90fdf5188e4d8b96726f6d9ce343955a2
Priyanshu <priyxsh005@gmail.com>
2026-01-07 09:57:50 +0530

    [Priyanshu] : Added PeakElement code

---
+++ dsa-practice/gcr-codebase/JavaDsaPactice/src/BInarySearch/PeakElement.java
@@ -0,0 +1,33 @@
+package BInarySearch;
+
+public class PeakElement {
+
+    public static int findPeakElement(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } 
+            else {
+                
+                right = mid;
+            }
+        }
+
+        return left; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+        int peakIndex = findPeakElement(arr);
+
+        System.out.println("Peak element index: " + peakIndex);
+        System.out.println("Peak element: " + arr[peakIndex]);
+    }
+}
+
