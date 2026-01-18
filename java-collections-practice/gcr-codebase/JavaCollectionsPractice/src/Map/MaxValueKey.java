97c83fbb08be5d50e2155b94c4e992668752f598
Priyanshu <priyxsh005@gmail.com>
2026-01-18 19:57:29 +0530

    [Priyanshu] : Added MaxValueKey code

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/Map/MaxValueKey.java
@@ -0,0 +1,28 @@
+package Map;
+
+import java.util.*;
+
+public class MaxValueKey {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 20);
+       map.put("C", 15);
+
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (String key : map.keySet()) {
+            int value = map.get(key);
+
+            if (value > maxValue) {
+                maxValue = value;
+                maxKey = key;
+            }
+        }
+
+        System.out.println("Key with highest value: " + maxKey);
+    }
+}
